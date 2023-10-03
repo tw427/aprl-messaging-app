cab8e84bc10277c20f8b24a6b267ecf45fd20dd4
Tony W <bobacrypto2@gmail.com>
2023-10-03 00:28:40 -0700

    Create login form and style login form

--- src/components/account-form.jsx
+++ src/components/account-form.jsx
@@ -3,38 +3,41 @@ import "../styles/account-form.css";
 const AccountForm = () => {
   return (
     <>
-      <label htmlFor="username">
-        <input
-          type="text"
-          id="username"
-          name="username"
-          placeholder="Username"
-        ></input>
-      </label>
-      <label htmlFor="password">
-        <input
-          type="password"
-          id="password"
-          name="password"
-          placeholder="Password"
-        ></input>
-      </label>
+      <label htmlFor="username"></label>
+      <input
+        type="text"
+        id="username"
+        name="username"
+        placeholder="Username"
+      ></input>
+      <label htmlFor="password"></label>
+      <input
+        type="password"
+        id="password"
+        name="password"
+        placeholder="Password"
+      ></input>
       {/* {newUser && (
         <>
           <label htmlFor="confirmPassword">
+          </label>
           <input
           type="password"
           id="confirmPassword"
           name="confirmPassword"
           placeholder="Confirm password"
           ></input>
-          </label>
           <button id="create-account-btn">Create Account</button>
         </>
       )} */}
-      <button type="submit" id="login-btn">
-        Login
-      </button>
+      <div id="form-buttons">
+        <button type="submit" id="login-btn">
+          Login
+        </button>
+        <a href="/" id="sign-up">
+          Create new account
+        </a>
+      </div>
     </>
   );
 };
