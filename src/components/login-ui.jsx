c5ff3ba4460f1b14209ae6f230350fc538da6cd4
Tony W <bobacrypto2@gmail.com>
2023-10-04 01:35:55 -0700

    Test responsive design in our app | Implement media queries

--- src/components/login-ui.jsx
+++ src/components/login-ui.jsx
@@ -1,10 +1,13 @@
 import "../styles/login.css";
 import AccountForm from "./account-form";
+import corgi from "../assets/cute-corgi.jpg";
 
 const LoginUI = () => {
   return (
     <div id="login">
-      <div id="login-image"></div>
+      <div id="login-image">
+        <img src={corgi}></img>
+      </div>
       <form id="account-form">
         <AccountForm />
       </form>
