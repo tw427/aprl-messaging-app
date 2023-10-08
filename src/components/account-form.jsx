be11a3a30ebe33fe93fa21cd214fd037af8e740a
Tony W <bobacrypto2@gmail.com>
2023-10-07 22:10:42 -0700

    Login form can now be toggled between creating an account and logging into an existing account

--- src/components/account-form.jsx
+++ src/components/account-form.jsx
@@ -1,6 +1,9 @@
+import { useState } from "react";
 import "../styles/account-form.css";
 
 const AccountForm = () => {
+  const [newUser, setNewUser] = useState(false);
+
   return (
     <>
       <label htmlFor="username"></label>
@@ -17,26 +20,50 @@ const AccountForm = () => {
         name="password"
         placeholder="Password"
       ></input>
-      {/* {newUser && (
+      {newUser && (
         <>
-          <label htmlFor="confirmPassword">
-          </label>
+          <label htmlFor="confirmPassword"></label>
           <input
-          type="password"
-          id="confirmPassword"
-          name="confirmPassword"
-          placeholder="Confirm password"
+            type="password"
+            id="confirmPassword"
+            name="confirmPassword"
+            placeholder="Confirm password"
           ></input>
-          <button id="create-account-btn">Create Account</button>
         </>
-      )} */}
+      )}
       <div id="form-buttons">
-        <button type="submit" id="login-btn">
-          Login
-        </button>
-        <a href="/" id="sign-up">
-          Create new account
-        </a>
+        {!newUser && (
+          <>
+            <button type="submit" id="login-btn">
+              Login
+            </button>
+            <a
+              href="/"
+              id="sign-up"
+              onClick={(e) => {
+                e.preventDefault();
+                setNewUser(true);
+              }}
+            >
+              Create new account
+            </a>
+          </>
+        )}
+        {newUser && (
+          <>
+            <button id="create-account-btn">Create Account</button>
+            <a
+              href="/"
+              id="sign-in"
+              onClick={(e) => {
+                e.preventDefault();
+                setNewUser(false);
+              }}
+            >
+              Sign in
+            </a>
+          </>
+        )}
       </div>
     </>
   );
