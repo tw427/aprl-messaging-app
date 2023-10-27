bfa3635ca10d54defbc9ed688e88f9f7f3f1d5d7
Tony W <bobacrypto2@gmail.com>
2023-10-26 18:03:04 -0700

    Create login fetch request function | Write comments on what to do when a login succeeds or fails

--- utils/userCrud.js
+++ utils/userCrud.js
@@ -12,3 +12,23 @@ export async function createUser(e) {
 
   return await res;
 }
+
+// Depending on if the login fetch is successful
+// if Successful we will reroute our users into the Home component
+// if Fail we will not reroute our user, and instead throw back an error message
+
+export async function login(e) {
+  if (e) {
+    e.preventDefault();
+  }
+
+  const formData = new FormData(document.getElementById("account-form"));
+  const data = new URLSearchParams(formData);
+  const res = await fetch("http://localhost:3001/user/login", {
+    method: "POST",
+    mode: "cors",
+    body: data,
+  });
+
+  return await res;
+}
