f0114ed64e1b9a98a2e207187258b19ab67f15f3
Tony W <bobacrypto2@gmail.com>
2023-11-07 04:42:33 -0800

    Success in retrieving messages with a GET request to our Backend

--- src/components/home/chat-window.jsx
+++ src/components/home/chat-window.jsx
@@ -1,13 +1,37 @@
 import "../../styles/home/chat-window.css";
+import { useState, useEffect } from "react";
 
 const ChatWindow = () => {
+  const [messages, setMessages] = useState({});
+
+  useEffect(() => {
+    async function getUserMessages() {
+      if (sessionStorage.id) {
+        const res = await fetch(
+          `http://localhost:3001/user/${sessionStorage.id}/messages`,
+          {
+            method: "GET",
+            mode: "cors",
+          }
+        );
+        setMessages(await res.json());
+      }
+      return;
+    }
+
+    getUserMessages();
+  }, []);
+
   return (
     <>
       <main id="chat-window">
-        <p>Window Message 1</p>
-        <p>Window Message 2</p>
-        <p>Window Message 3</p>
-        <p>Window Message 4</p>
+        <button
+          onClick={() => {
+            console.log(messages);
+          }}
+        >
+          Messages
+        </button>
       </main>
     </>
   );
