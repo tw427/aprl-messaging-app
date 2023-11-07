02fabdc999a142432a5aef1bee63b500bea0f3d5
Tony W <bobacrypto2@gmail.com>
2023-11-07 03:58:19 -0800

    Add form input to send messages in our message-window component | Add logic to make a POST request to our backend to create messages

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -12,13 +12,8 @@ const Home = () => {
 
   useEffect(() => {
     console.log(user);
-    // Session Storage availability check
-    // if (storageAvailable("sessionStorage")) {
-    //   console.log("Horray! Session Storage is available.");
-    // } else {
-    //   console.log("Uh oh something went wrong!");
-    // }
   }, [user]);
+
   return (
     <>
       <motion.div
@@ -27,11 +22,6 @@ const Home = () => {
         animate={{ opacity: 1 }}
         exit={{ opacity: 0 }}
       >
-        {/* {user.user && (
-          <Link to={"/"} onClick={() => console.log(user)}>
-            APR3L Messaging App
-          </Link>
-        )} */}
         {user && (
           <>
             <Profile />
--- src/components/home/message-window.jsx
+++ src/components/home/message-window.jsx
@@ -1,10 +1,47 @@
 import "../../styles/home/message-window.css";
 
 const MessageWindow = () => {
+  function clearMessageInput() {
+    const message = document.getElementById("message");
+    message.value = "";
+  }
+  async function postMessage(id, e) {
+    if (e) {
+      e.preventDefault();
+    }
+
+    const formData = new FormData(document.getElementById("message-form"));
+    const data = new URLSearchParams(formData);
+    const res = await fetch(`http://localhost:3001/user/create-message/${id}`, {
+      method: "POST",
+      mode: "cors",
+      body: data,
+    });
+
+    console.log(await res.json());
+
+    // return await res;
+  }
+
   return (
     <>
       <section id="message-window">
-        <p>Message window</p>
+        <form
+          id="message-form"
+          onSubmit={(e) => {
+            if (sessionStorage.id) {
+              postMessage(sessionStorage.id, e);
+              clearMessageInput();
+            } else {
+              return;
+            }
+          }}
+        >
+          <label htmlFor="message">
+            <input type="text" id="message" name="message" />
+          </label>
+          <button type="submit">Send</button>
+        </form>
       </section>
     </>
   );
