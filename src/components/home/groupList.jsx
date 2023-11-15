00690198fcebcc2ff6f113532ce93cec898c3640
Tony W <bobacrypto2@gmail.com>
2023-11-15 01:45:45 -0800

    Add buttons to toggle between User list and Group list view / components

--- src/components/home/groupList.jsx
+++ src/components/home/groupList.jsx
@@ -1,7 +1,19 @@
-const GroupList = () => {
+import { PropTypes } from "prop-types";
+
+const GroupList = (props) => {
+  const { showUserList, setShowUserList } = props;
+
+  GroupList.propTypes = {
+    showUserList: PropTypes.string,
+    setShowUserList: PropTypes.func,
+  };
   return (
     <>
-      <div id="group-list"></div>
+      <div id="group-list">
+        <button onClick={() => setShowUserList(!showUserList)}>
+          User List
+        </button>
+      </div>
     </>
   );
 };
--- src/components/home/userlist.jsx
+++ src/components/home/userlist.jsx
@@ -2,16 +2,19 @@ import PropTypes from "prop-types";
 import "../../styles/home/user-list.css";
 
 const UserList = (props) => {
-  const { toggleView } = props;
+  const { showUserList, setShowUserList } = props;
 
   UserList.propTypes = {
-    toggleView: PropTypes.func,
+    showUserList: PropTypes.string,
+    setShowUserList: PropTypes.func,
   };
 
   return (
     <>
       <section id="user-list">
-        <button onClick={() => toggleView()}>Group List</button>
+        <button onClick={() => setShowUserList(!showUserList)}>
+          Group List
+        </button>
         <div className="userlist-user">User List 1</div>
         <div className="userlist-user">User List 2</div>
         <div className="userlist-user">User List 3</div>
